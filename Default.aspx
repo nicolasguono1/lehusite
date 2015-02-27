1d6cde07f2ed8a148d562594e47797d9a2ad823a
nicolasguono1 <sihezhan@sina.com>
2015-02-27 17:43:23 +0800

    初版

---
+++ Default.aspx
@@ -0,0 +1,55 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" Async="true" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        
+    </div>
+        <asp:DropDownList ID="ddl_type" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem Selected="True" Value="picture">图片</asp:ListItem>
+            <asp:ListItem Value="user">用户</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <asp:GridView ID="gridview_picture" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+            <Columns>
+                <asp:BoundField DataField="用户名" HeaderText="用户名" />
+                <asp:BoundField DataField="举报数" HeaderText="举报数" />
+                <asp:ImageField DataImageUrlField="图片" HeaderText="图片">
+                </asp:ImageField>
+                <asp:TemplateField ShowHeader="False">
+                    <ItemTemplate>
+                        <asp:LinkButton ID="LinkButton1" runat="server" CausesValidation="False" CommandArgument='<%# Eval("objectId") %>' OnClick="LinkButton1_Click" Text="删除图片" OnClientClick="javascript:alert('删除成功！');"></asp:LinkButton>
+                    </ItemTemplate>
+                    <ItemStyle Font-Size="Small" />
+                </asp:TemplateField>
+            </Columns>
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+        <asp:GridView ID="gridview_user" runat="server" BackColor="White" BorderColor="#3366CC" BorderStyle="None" BorderWidth="1px" CellPadding="4">
+            <FooterStyle BackColor="#99CCCC" ForeColor="#003399" />
+            <HeaderStyle BackColor="#003399" Font-Bold="True" ForeColor="#CCCCFF" />
+            <PagerStyle BackColor="#99CCCC" ForeColor="#003399" HorizontalAlign="Left" />
+            <RowStyle BackColor="White" ForeColor="#003399" />
+            <SelectedRowStyle BackColor="#009999" Font-Bold="True" ForeColor="#CCFF99" />
+            <SortedAscendingCellStyle BackColor="#EDF6F6" />
+            <SortedAscendingHeaderStyle BackColor="#0D4AC4" />
+            <SortedDescendingCellStyle BackColor="#D6DFDF" />
+            <SortedDescendingHeaderStyle BackColor="#002876" />
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using AVOSCloud;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private DataTable datasource = null;
+    private string report_count_key = "report_count";
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        AVClient.Initialize("u2usgtzl5t8w9t2qpf8bbc88rvg85g5tgjtja3jpq0gfoilc", "dspdy8ip356aahviafq216hszwb0gp908vov9b4cck7nrywu");
+        this.InitPictures();
+    }
+
+    async private void InitPictures()
+    {
+        IEnumerable<AVObject> blogs = null;
+        var task = AVObject.GetQuery("Blog").WhereGreaterThan(this.report_count_key, 0).OrderByDescending(this.report_count_key).FindAsync().ContinueWith(r => 
+        {
+            blogs = r.Result;
+            datasource = new DataTable();
+            datasource.Columns.Add("用户名");
+            datasource.Columns.Add("举报数");
+            datasource.Columns.Add("图片");
+            datasource.Columns.Add("objectId");
+
+            foreach (var blog in blogs)
+            {
+                datasource.Rows.Add(new[] { blog["username"], blog["report_count"], ((AVFile)blog["image"]).Url, blog.ObjectId });
+            }
+
+            this.gridview_picture.DataSource = datasource;
+            this.gridview_picture.DataBind();
+            this.gridview_user.Visible = false;
+            this.gridview_picture.Visible = true;
+        });
+
+        await task;
+    }
+
+    async private void InitUsers()
+    {
+        IEnumerable<AVUser> users = null;
+        var task = AVUser.Query.WhereGreaterThan(this.report_count_key, 0).OrderByDescending(this.report_count_key).FindAsync().ContinueWith(r =>
+        {
+            users = r.Result;
+            datasource = new DataTable();
+            datasource.Columns.Add("用户名");
+            datasource.Columns.Add("被举报数");
+            
+            foreach (var user in users)
+            {
+                datasource.Rows.Add(new[] { user["nickname"], user["report_count"] });
+            }
+
+            this.gridview_user.DataSource = datasource;
+            this.gridview_user.DataBind();
+            this.gridview_picture.Visible = false;
+            this.gridview_user.Visible = true;
+        });
+
+        await task;
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        if (this.ddl_type.SelectedIndex == 0)
+        {
+            this.InitPictures();
+        }
+        else
+        {
+            this.InitUsers();
+        }
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        LinkButton button = (LinkButton)sender;
+        this.DeletePicture(button.CommandArgument);
+    }
+
+    async private void DeletePicture(string objectId)
+    {
+        AVObject blog = null;
+        await AVObject.GetQuery("Blog").GetAsync(objectId).ContinueWith(r =>
+            {
+                blog = r.Result;
+                blog.DeleteAsync();
+            });
+
+    }
+}
